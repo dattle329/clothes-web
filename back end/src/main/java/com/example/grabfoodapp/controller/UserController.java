4fcf0561b6026ff2605d5767a498a1501d91d6e2
Le Tuan Dat <dattle329@gmail.com>
2024-01-09 11:17:45 +0700

    [Add] new code

--- back end/src/main/java/com/example/grabfoodapp/controller/UserController.java
+++ back end/src/main/java/com/example/grabfoodapp/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.grabfoodapp.controller;
 
 import com.example.grabfoodapp.dto.UserDto;
+import com.example.grabfoodapp.entity.User;
 import com.example.grabfoodapp.form.UserCreateForm;
 import com.example.grabfoodapp.form.UserUpdateForm;
 import com.example.grabfoodapp.service.UserService;
@@ -8,8 +9,12 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @AllArgsConstructor
@@ -41,5 +46,18 @@ public class UserController {
     @DeleteMapping("/api/v1/user/{id}")
     public void delete(@PathVariable("id") Long id) {
         userService.delete(id);
+    }
+
+    @GetMapping("api/v1/login")
+    public ResponseEntity<?> login(Principal principal) {
+        String username = principal.getName();
+        // Tim account tương ứng dựa vào username
+        User userLogin = userService.findUserByUsername(username);
+        UserDto userDTO = new UserDto();
+        userDTO.setId(userLogin.getId());
+        userDTO.setUsername(userLogin.getUsername());
+        userDTO.setEmail(userLogin.getEmail());
+        userDTO.setName(userLogin.getName());
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 }
